e9d249539111bbbe5ed52139e7e4fbafabd2d478
ncolyer <90807490+ncolyer11@users.noreply.github.com>
2023-04-20 14:12:45 +1000

    Add files via upload
    src :)

---
+++ src/main/java/net/fabricmc/example/rtf/mixin/HugeFungusFeatureMixin.java
@@ -0,0 +1,38 @@
+package net.fabricmc.example.rtf.mixin;
+
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.gen.feature.HugeFungusFeature;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(HugeFungusFeature.class)
+abstract
+class HugeFungusFeatureMixin {
+	
+	private static final float NEW_TALL_FUNGUS_CHANCE = 0.01F;
+
+	@ModifyArg(method = "generate", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/random/Random;nextInt(I)I"))
+	private int increaseTallFungusChance(int bound) {
+		return (int) (1 / NEW_TALL_FUNGUS_CHANCE);
+	}
+
+	@ModifyArg(method = "generateHat", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/gen/feature/HugeFungusFeature;placeHatBlock(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/random/Random;Lnet/minecraft/world/gen/feature/HugeFungusFeatureConfig;Lnet/minecraft/util/math/BlockPos$Mutable;FFF)V")
+			, index = 4)
+	private float increaseFungusShroomBloat(float decorationChance) {
+		return 0;
+	}
+
+	@ModifyArg(method = "generateHat", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/gen/feature/HugeFungusFeature;placeHatBlock(Lnet/minecraft/world/WorldAccess;Lnet/minecraft/util/math/random/Random;Lnet/minecraft/world/gen/feature/HugeFungusFeatureConfig;Lnet/minecraft/util/math/BlockPos$Mutable;FFF)V")
+			, index = 5)
+	private float increaseFungusWartBloat(float generationChance) {
+		return 1;
+	}
+
+	@Redirect(method = "placeWithOptionalVines", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/random/Random;nextFloat()F"))
+	private float maxVRM(Random instance) {
+		return 0.0F;
+	}
+
+}
